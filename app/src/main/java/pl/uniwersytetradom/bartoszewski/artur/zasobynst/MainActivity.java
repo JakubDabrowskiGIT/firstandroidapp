efab492f3c886af1c0dcd65998270df238df4d89
mrkubek <crimson.kuba@gmail.com>
2017-11-19 17:24:31 +0100

    first commit

---
+++ app/src/main/java/pl/uniwersytetradom/bartoszewski/artur/zasobynst/MainActivity.java
@@ -0,0 +1,39 @@
+package pl.uniwersytetradom.bartoszewski.artur.zasobynst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView pic;
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pic = (ImageView) findViewById(R.id.imageView01);
+        title = (TextView) findViewById(R.id.texView01);
+        pic.setImageResource(R.drawable.images);
+
+
+    }
+
+    public void opis2 (View view) {
+        pic.setImageResource(R.drawable.wilk01);
+        title.setText(R.string.opis1);
+    }
+
+    public void opis1 (View view) {
+        pic.setImageResource(R.drawable.images);
+        title.setText(R.string.napis);
+    }
+
+    public void opis3 (View view) {
+        pic.setImageResource(R.drawable.italy);
+        title.setText(R.string.opis2);
+    }
+}
